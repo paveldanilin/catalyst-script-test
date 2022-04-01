5ec0aa8d9991fea686f5cc94cc730ec8d764e908
Pavel <pavel.k.danilin@gmail.com>
2022-04-01 11:43:08 +1000

    first commit

---
+++ src/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pada\CatalystScriptTest;
+
+final class App
+{
+    public function run(): void
+    {
+        print "RUN\n";
+    }
+}
---
+++ user_upload.php
@@ -0,0 +1,7 @@
+<?php
+
+use Pada\CatalystScriptTest\App;
+
+require "./vendor/autoload.php";
+
+(new App())->run();
